fd3201a5662d6db5c2204d6d36b2493a3882cd9c
Plínio Mayer <pctmayer@gmail.com>
2025-04-07 10:08:10 -0300

    fix: deploy permissions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,19 +8,16 @@ scp -r \
     favicon.ico \
     package-lock.json \
     package.json \
-    public/ \
     src/ \
     tsconfig.json \
     root@vps56603.publiccloud.com.br:/opt/backlinio/;
 
 ssh root@vps56603.publiccloud.com.br "/usr/bin/bash -s" << EOF
     npm --prefix /opt/backlinio/ i;
-    chmod 755 -R /opt/backlinio;
-    screen -X -S strapi quit;
-    screen -S strapi -dm npm --prefix /opt/backlinio/ run start;
-EOF
-
-ssh strapi@vps56603.publiccloud.com.br "/usr/bin/bash -s" << EOF
+    mkdir -p /opt/backlinio/public/uploads;
+    chmod 750 -R /opt/backlinio;
+    chmod 660 -R /opt/backlinio/public/uploads;
+    chmod 770 /opt/backlinio/public/uploads;
     screen -X -S strapi quit;
     screen -S strapi -dm npm --prefix /opt/backlinio/ run start;
 EOF
