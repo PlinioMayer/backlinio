8cece4f572e5803ee05b0f185182742452b7dfca
Plínio Mayer <pctmayer@gmail.com>
2025-04-09 19:56:50 -0300

    feat: date utils endpoint

---
+++ src/api/utils/controller/utils.ts
@@ -0,0 +1,5 @@
+import { format } from "../../../utils";
+
+export const today = (): string => {
+    return format(new Date());
+  }
---
+++ src/api/utils/routes/utils.ts
@@ -0,0 +1,13 @@
+import { today } from "../controller/utils";
+
+export default [
+  {
+    
+    method: 'GET',
+    path: '/api/utils/today',
+    handler: today,
+    config: {
+      auth: false
+    },
+  }
+]
